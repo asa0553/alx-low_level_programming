ee7ba42ab255176f57b53d150ded60c38a288283
asa0553 <aobed117@gmail.com>
2022-06-29 10:46:51 -0700

    pust

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
-#include <stdio.o>
+#include <stdio.h>
 #include <string.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+ *   main - main block
+ *   Return: 0
+ *    
 
 int main(void)
 {
-	puts("\"Programing is like building a multilingua puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-
-
-
 }
-
