de3a24a87c8bf29e1dd669b99c8d08e313e4a757
asa0553 <aobed117@gmail.com>
2022-07-12 10:20:33 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ *
+ * @s: value
+ */
+void rev_string(char *s)
+{
+	int a;
+	int b;
+	int c;
+	char d;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		;
+	}
+	c = a;
+	for (a--, b = 0; b < 1 / 2; a--, b++)
+	{
+		d = s[b];
+		s[b] = s[a];
+		s[a] = d;
+	}
+}
